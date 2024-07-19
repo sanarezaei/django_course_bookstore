085b51507b2dc0e89cbd3eb4608735505728ac6b
= <sanare1382@gmail.com>
2024-07-19 14:44:01 +0330

    add book create, detail, update and delelte view and templates

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Book
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
+
 
 class BookListView(generic.ListView):
     model = Book
@@ -10,3 +13,19 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+    
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_create.html'
+    
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_update.html'
+    
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy("book_list")
+    
