45d5d8561b26ef83734e61e7f141dd843e2e1f1f
= <sanare1382@gmail.com>
2024-07-21 10:35:56 +0330

    add comment model

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Comment
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'datetime_created') 
+
+
+# admin.site.register(Comment)
 admin.site.register(Book)
